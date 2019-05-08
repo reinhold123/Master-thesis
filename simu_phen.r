a31100e7e764423189a7a7186885361b692dda89
Reinhold Saiz <reinhold.saiz@stud-mail.uni-wuerzburg.de>
2019-05-08 09:54:47 +0200

    epistatic model got major overhaul

--- simu_phen.r
+++ simu_phen.r
@@ -1,8 +1,8 @@
 ###################### R script for simulating additive phenotypes from real genomic data of A. thaliana
 
 simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_order=2, epistatic_variance_explained=0.5, report=TRUE, model="additive"){
-  if(model != "additive" & model != "epistatic"){
-    cat("Mission aborted. Model has to be either additive or epistatic.\n")
+  if(model != "additive" & model != "epistatic" & model != "mixed"){
+    cat("Mission aborted. Model has to be either mixed, additive or epistatic.\n")
     return(NULL)
   }
   X <- X
@@ -30,42 +30,102 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
           genomic_value[j] <- genomic_value[j] + beta * X[j,i]}
       }
       prel_phenotype_value <- latitude_value * genomic_value
-    
+  
       noise_value <- var(genomic_value) * (1-herit)/herit
       prel_phenotype_value <- prel_phenotype_value + rnorm(length(prel_phenotype_value), sd=sqrt(noise_value))
       count <- count + 1
     }
   }
-  if(model == "epistatic"){
-    while(count < 5){
-      genomic_value <- rep(0, length(rownames(X)))
-      for (i in effective_markers){
-        beta <- sqrt((veb*var(na.omit(prel_phenotype_value))) / ((var(X[,i])) - (veb * var(X[,i]))))
-        for (j in 1:length(genomic_value)){
-          genomic_value[j] <- genomic_value[j] + beta * X[j,i]}
-      }
-      add_phenotype_value <- latitude_value * genomic_value
-      num_markers <- epistatic_order
-      epistatic_variance_explained <- epistatic_variance_explained
-      epistatic_markers <- sample(ncol(X), size=num_markers)
+  
+  if(model == "epistatic" | model == "mixed"){
+    andmarkers <- as.data.frame(matrix(nrow=nrow(Y_temp), ncol=1000))
+    ormarkers <- as.data.frame(matrix(nrow=nrow(Y_temp), ncol=1000))
     
-      epistatic_value <- rep(0, length(rownames(X)))
-      for (i in epistatic_markers){
-        #beta <- sqrt((veb*var(na.omit(add_phenotype_value))) / ((var(X[,i])) - (veb * var(X[,i]))))
-        for (j in 1:length(epistatic_value)){
-          if(X[j,i] == 1){
-            epistatic_value[j] <- 1
+    for (j in 1:1000){
+      repeat {
+        epistatic_markers <- sample(ncol(X), size=2)
+        for (i in c(1,2)){
+          assign(paste0("bar", i), sum(X[, epistatic_markers[i]])/nrow(X))
+          if (get(paste0("bar", i)) > 1-get(paste0("bar", i))){
+            assign(paste0("MAF", i), 1-get(paste0("bar", i)))
           }
           else{
-            epistatic_value[j] <- 0
+            assign(paste0("MAF", i), get(paste0("bar", i)))
           }
         }
+        if (MAF1 > 0.3 & MAF1 < 0.5 & MAF2 > 0.3 & MAF2 < 0.5) break
       }
-      prel_phenotype_value <- add_phenotype_value + epistatic_variance_explained*epistatic_value
-      noise_value <- var(na.omit(add_phenotype_value)) * (1-herit)/herit
-      prel_phenotype_value <- prel_phenotype_value + rnorm(length(prel_phenotype_value), sd=sqrt(noise_value))
-      count <- count + 1
+      
+      foo <- as.data.frame(matrix(nrow=nrow(Y_temp), ncol=4))
+      colnames(foo) <- c("marker1", "marker2", "andmarker", "ormarker")
+      foo$marker1 <- X[, epistatic_markers[1]]
+      foo$marker2 <- X[, epistatic_markers[2]]
+      
+      for (i in 1:length(foo$marker1)){
+        if (foo$marker1[i] == 1 & foo$marker2[i] == 1){
+          foo$andmarker[i] <- 1
+        }
+        else{
+          foo$andmarker[i] <- 0
+        }
+        if (foo$marker1[i] == 1 | foo$marker2[i] == 1){
+          foo$ormarker[i] <- 1
+        }
+        else{
+          foo$ormarker[i] <- 0
+        }
+      }
+      andmarkers[, j] <- foo$andmarker
+      ormarkers [, j] <- foo$ormarker
     }
+    
+    snp_mean <- apply(as.matrix(ormarkers), 2, mean)
+    snp_sd <- apply(as.matrix(ormarkers), 2, sd)
+    snpm <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_mean, byrow=T)
+    snpd <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_sd, byrow=T)
+    xx_gut <- (as.matrix(ormarkers)-snpm)/snpd
+    
+    snp_mean <- apply(X,2,mean)
+    snp_sd <- apply(X,2,sd)
+    snpm <- matrix(nrow=nrow(X), ncol=ncol(X), data=snp_mean, byrow=T)
+    snpd <- matrix(nrow=nrow(X), ncol=ncol(X), data=snp_sd, byrow=T)
+    xx_stand <- (X-snpm)/snpd
+    
+    or_r2 <- (crossprod(xx_gut, xx_stand)/nrow(X))^2
+    
+    snp_mean <- apply(as.matrix(andmarkers), 2, mean)
+    snp_sd <- apply(as.matrix(andmarkers), 2, sd)
+    snpm <- matrix(nrow=nrow(andmarkers), ncol=ncol(andmarkers), data=snp_mean, byrow=T)
+    snpd <- matrix(nrow=nrow(andmarkers), ncol=ncol(andmarkers), data=snp_sd, byrow=T)
+    xx_gut <- (as.matrix(andmarkers)-snpm)/snpd
+    
+    and_r2 <- (crossprod(xx_gut, xx_stand)/nrow(X))^2
+    
+    rm(snp_mean, snp_sd, snpm, snpd, xx_stand, xx_gut)
+    
+    and_rms <- NULL
+    or_rms <- NULL
+    
+    for (k in 1:nrow(and_r2)){
+      if (any(and_r2[k,] > 0.5)){
+        and_rms[k] <- k
+      }
+      if (any(or_r2[k,] > 0.5)){
+        or_rms[k] <- k
+      }
+    }
+    
+    andmarkers <- andmarkers[, -na.omit(and_rms)]
+    ormarkers <- ormarkers[, -na.omit(or_rms)]
+    
+    foo <- sample(ncol(andmarkers), size=5)
+    bar <- sample(ncol(ormarkers), size=5)
+    prel_phenotype_value <- andmarkers[,foo] + ormarkers[,bar]
+  }
+  
+  if(model == "mixed"){
+    cat("Mixed model not yet implemented. Choose either additive or epistatic model.\n")
+    return(NULL)
   }
   
   phenotype_value <- prel_phenotype_value
