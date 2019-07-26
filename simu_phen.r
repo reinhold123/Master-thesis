973491ddd50c575b9c7d48d639c2d36da4793c96
Reinhold Saiz <reinhold.saiz@stud-mail.uni-wuerzburg.de>
2019-07-26 17:05:27 +0200

    Updated all functions; heritability works better now, GBLUP now works as intended

--- simu_phen.r
+++ simu_phen.r
@@ -9,8 +9,16 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
   Y_temp <- Y
   K <- K
   
-  latitude_value <- 0.5 + (((Y_temp$accession_latitude - min(na.omit(Y_temp$accession_latitude)))*0.5)/
-                             (max(na.omit(Y_temp$accession_latitude)) - min(na.omit(Y_temp$accession_latitude))))
+  #latitude_value <- 0.5 + (((Y_temp$accession_latitude - min(na.omit(Y_temp$accession_latitude)))*0.5)/
+  #                           (max(na.omit(Y_temp$accession_latitude)) - min(na.omit(Y_temp$accession_latitude))))
+  latitude_value <- Y_temp$accession_latitude
+  lat <- na.omit(latitude_value)
+  for (k in 1:length(latitude_value)){
+    if ((k %% 2) == 0){
+      latitude_value[k] <- mean(lat)
+    }
+  }
+  rm(lat)
   ecotype <- as.numeric(Y_temp$accession_id)
   rownames(X)<-ecotype
   
@@ -22,14 +30,14 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
   prel_phenotype_value <- latitude_value
   
   if(model == "additive" | model == "mixed"){
-    while(count < 3){
+    while(count < 5){
       genomic_value <- rep(0, length(rownames(X)))
       for (i in effective_markers){
         beta <- sqrt((veb*var(na.omit(prel_phenotype_value))) / ((var(X[,i])) - (veb * var(X[,i]))))
         for (j in 1:length(genomic_value)){
           genomic_value[j] <- genomic_value[j] + beta * X[j,i]}
       }
-      prel_phenotype_value <- latitude_value * genomic_value
+      prel_phenotype_value <- latitude_value + genomic_value
   
       noise_value <- var(genomic_value) * (1-herit)/herit
       prel_phenotype_value <- prel_phenotype_value + rnorm(length(prel_phenotype_value), sd=sqrt(noise_value))
@@ -69,22 +77,22 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
         else{
           foo$andmarker[i] <- 0
         }
-        if (foo$marker1[i] == 1 | foo$marker2[i] == 1){
-          foo$ormarker[i] <- 1
-        }
-        else{
-          foo$ormarker[i] <- 0
-        }
+        #if (foo$marker1[i] == 1 | foo$marker2[i] == 1){
+         # foo$ormarker[i] <- 1
+        #}
+        #else{
+         # foo$ormarker[i] <- 0
+        #}
       }
       andmarkers[, j] <- foo$andmarker
-      ormarkers [, j] <- foo$ormarker
+      #ormarkers [, j] <- foo$ormarker
     }
     
-    snp_mean <- apply(as.matrix(ormarkers), 2, mean)
-    snp_sd <- apply(as.matrix(ormarkers), 2, sd)
-    snpm <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_mean, byrow=T)
-    snpd <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_sd, byrow=T)
-    xx_gut <- (as.matrix(ormarkers)-snpm)/snpd
+    #snp_mean <- apply(as.matrix(ormarkers), 2, mean)
+    #snp_sd <- apply(as.matrix(ormarkers), 2, sd)
+    #snpm <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_mean, byrow=T)
+    #snpd <- matrix(nrow=nrow(ormarkers), ncol=ncol(ormarkers), data=snp_sd, byrow=T)
+    #xx_gut <- (as.matrix(ormarkers)-snpm)/snpd
     
     snp_mean <- apply(X,2,mean)
     snp_sd <- apply(X,2,sd)
@@ -92,7 +100,7 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
     snpd <- matrix(nrow=nrow(X), ncol=ncol(X), data=snp_sd, byrow=T)
     xx_stand <- (X-snpm)/snpd
     
-    or_r2 <- (crossprod(xx_gut, xx_stand)/nrow(X))^2
+    #or_r2 <- (crossprod(xx_gut, xx_stand)/nrow(X))^2
     
     snp_mean <- apply(as.matrix(andmarkers), 2, mean)
     snp_sd <- apply(as.matrix(andmarkers), 2, sd)
@@ -105,32 +113,31 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
     rm(snp_mean, snp_sd, snpm, snpd, xx_stand, xx_gut)
     
     and_rms <- NULL
-    or_rms <- NULL
+    #or_rms <- NULL
     
     for (k in 1:nrow(and_r2)){
       if (any(and_r2[k,] > 0.5)){
         and_rms[k] <- k
       }
-      if (any(or_r2[k,] > 0.5)){
-        or_rms[k] <- k
-      }
+      #if (any(or_r2[k,] > 0.5)){
+      #  or_rms[k] <- k
+      #}
     }
     
     andmarkers <- andmarkers[, -na.omit(and_rms)]
-    ormarkers <- ormarkers[, -na.omit(or_rms)]
+    #ormarkers <- ormarkers[, -na.omit(or_rms)]
     
-    repeat{
-      foo <- sample(ncol(andmarkers), size=5)
-      bar <- sample(ncol(ormarkers), size=5)
-      epistatic_value <- andmarkers[,foo[1]] + andmarkers[,foo[2]] + andmarkers[,foo[3]] + andmarkers[,foo[4]] + andmarkers[,foo[5]] +
-        ormarkers[,bar[1]] + ormarkers[,bar[2]] + ormarkers[,bar[3]] + ormarkers[,bar[4]] + ormarkers[,bar[5]]
-      Y <- as.data.frame(ecotype, epistatic_value)
-      h2test <- amm_gwas(Y=Y, X=X, K=K2029, run=FALSE, report=FALSE)
-      if(h2test[1] < 0.3) break
-    }
+    foo <- sample(ncol(andmarkers), size=5)
+    #bar <- sample(ncol(ormarkers), size=5)
+    epistatic_value <- andmarkers[,foo[1]] + andmarkers[,foo[2]] + andmarkers[,foo[3]] + andmarkers[,foo[4]] + andmarkers[,foo[5]] 
+    #+ ormarkers[,bar[1]] + ormarkers[,bar[2]] + ormarkers[,bar[3]] + ormarkers[,bar[4]] + ormarkers[,bar[5]]
     
     if(model == "epistatic"){
-      prel_phenotype_value <- epistatic_value
+      latitude_value <- 0.5 + (((latitude_value - min(na.omit(latitude_value)))*0.5/
+                                  max(na.omit(latitude_value) - min(na.omit(latitude_value)))))
+      prel_phenotype_value <- latitude_value + epistatic_value
+      noise_value <- var(epistatic_value) * (1-herit)/herit
+      prel_phenotype_value <- prel_phenotype_value + rnorm(length(prel_phenotype_value), sd=sqrt(noise_value))
     }
   }
   
@@ -145,6 +152,7 @@ simu_phen <- function(X, Y, K, herit=0.5, num_additive_markers=5, epistatic_orde
     cat("Brace yourself, results are coming...\n")
     h2test <- amm_gwas(Y, X, K, run=FALSE, report=FALSE)
     cat("Set heritability was", herit, "\t estimated heritability is", h2test[1], "\n")
+    Y <- list(Y, h2test[1])
   }
   return(Y)
 }
